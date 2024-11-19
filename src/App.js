c52894e3da267d49b40ef3bc0668823a92f13cbe
fran0770 <franmowat33@gmail.com>
2024-11-19 12:26:17 +0000

    integrating styles and cart

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { useEffect, useState } from 'react';
 import SearchBar from './Components/SearchBar';
 import BookList from './Components/BookList';
 import FavouriteList from './Components/FavouriteList';
+import Cart from './Components/Cart';
+import './App.css';
 
 const App = () => {
   const [books, setBooks] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [favourites, setFavourites] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
 
   //fetch data from books.json
   useEffect(() => { 
@@ -24,8 +27,7 @@ const App = () => {
   );
 
   const handleAddToFavourites = (book) => {
-    console.log(favourites.book);
-    if (!favourites.includes(book)){
+    if (!favourites.some((favourites) => favourites.title === book.title)){
       setFavourites([...favourites, book]);
     }
   };
@@ -50,10 +52,19 @@ const App = () => {
   }, []);
 
   return (
-    <div>
-      <SearchBar handleSearch={handleSearch} />
-      <BookList books={filteredBooks} handleAddToFavourites={handleAddToFavourites}/>
-      <FavouriteList favourites={favourites} handleClearFavourites={handleClearFavourites}/>
+    <div className='app'>
+      <div className='searchbar-container'>
+        <SearchBar handleSearch={handleSearch} />
+      </div>
+      <div className='main-container'>
+        <div className='left-section'>
+          <FavouriteList favourites={favourites} handleClearFavourites={handleClearFavourites}/>
+          <BookList books={filteredBooks} handleAddToFavourites={handleAddToFavourites} favourites={favourites} />
+        </div>
+        <div className='right-section'>
+          <Cart cartItems={cartItems} setCartItems={setCartItems} />
+        </div>
+      </div>
     </div>
   );
 }
